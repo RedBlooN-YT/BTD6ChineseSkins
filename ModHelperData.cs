fa58d0bdaa7db6b08fb06769c0e618fee90790e6
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-06-25 17:26:26 -0700

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ChineseSkins
+{
+	public static class ModHelperData
+	{
+		public const string WorksOnVersion = "55.2";
+
+		public const string Version = "2.0.0";
+
+		public const string Name = "Chinese Skins";
+
+		public const string Description = "Adds the skins from the chinese edition of the game.";
+
+		public const string RepoOwner = "RedBlooN-YT";
+
+		public const string RepoName = "BTD6ChineseSkins";
+	}
+}
